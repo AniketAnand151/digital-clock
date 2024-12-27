6ad9c2d18b4a1879af202983d0afbba730762a1d
Aniket Anand <Aniketanand256@gmail.com>
2024-12-27 19:44:19 +0530

    Add files via upload

---
+++ digital clock/clock.js
@@ -0,0 +1,27 @@
+const clock = document.getElementById('clock');
+
+setInterval(function(){
+    let date = new Date()
+    Clock.innerHTML = date.toLocaleTimeString();
+},1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
